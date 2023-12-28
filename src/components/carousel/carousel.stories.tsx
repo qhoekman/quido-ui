ae48be3edfc9443d57d0df53084dec6b4ed8a9b4
Quido Hoekman <quido.hoekman@dij.digital>
2023-12-28 07:55:58 +0100

    feat(carousel): added component

---
+++ src/components/carousel/carousel.stories.tsx
@@ -0,0 +1,36 @@
+import { Card, CardContent } from "@/components/card/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/carousel/carousel";
+import type { Meta, StoryFn } from "@storybook/react";
+
+export default {
+  title: "Components/Media Display/Carousel",
+  component: Carousel,
+} as Meta<typeof Carousel>;
+
+export const Default: StoryFn = (args) => (
+  <div className="max-w-sm mx-auto">
+    <Carousel {...args}>
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="basis-1/3">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  </div>
+);
