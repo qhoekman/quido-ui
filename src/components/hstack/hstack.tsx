e4c14b0d1f5d582f68dc74868576f0794e488164
Quido Hoekman <quido.hoekman@dij.digital>
2023-12-28 10:07:09 +0100

    feat(hstack): added component

---
+++ src/components/hstack/hstack.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+const HStack = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentPropsWithoutRef<"div">
+>(({ children, className, ...props }, ref) => {
+  return (
+    <div ref={ref} className={cn("flex gap-4", className)} {...props}>
+      {children}
+    </div>
+  );
+});
+
+export { HStack };
