4c3712d227a172304154e002c644c2c190fceb1a
Quido Hoekman <quido.hoekman@dij.digital>
2023-12-28 10:07:27 +0100

    feat(vstack): added component

---
+++ src/components/vstack/vstack.stories.tsx
@@ -0,0 +1,20 @@
+import { Card, CardContent } from "@/components/card/card";
+import { VStack } from "@/components/vstack/vstack";
+import type { Meta, StoryFn } from "@storybook/react";
+
+export default {
+  title: "Components/Layout/VStack",
+  component: VStack,
+} as Meta<typeof VStack>;
+
+export const Default: StoryFn = (args) => (
+  <VStack {...args}>
+    {Array.from({ length: 5 }).map((_, index) => (
+      <Card key={index} className="w-32 bg-primary text-primary-fg">
+        <CardContent className="flex aspect-square items-center justify-center p-6">
+          <span className="text-4xl font-semibold">{index + 1}</span>
+        </CardContent>
+      </Card>
+    ))}
+  </VStack>
+);
---
+++ src/components/vstack/vstack.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+const VStack = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentPropsWithoutRef<"div">
+>(({ children, className, ...props }, ref) => {
+  return (
+    <div ref={ref} className={cn("flex flex-col gap-4", className)} {...props}>
+      {children}
+    </div>
+  );
+});
+
+export { VStack };
