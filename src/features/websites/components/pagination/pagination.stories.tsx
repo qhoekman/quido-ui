00461d393dd167c6200e1df47259c15eaf5ed2b2
Quido Hoekman <quido.hoekman@dij.digital>
2023-12-28 10:21:47 +0100

    feat(pagination): added component

---
+++ src/features/websites/components/pagination/pagination.stories.tsx
@@ -0,0 +1,42 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/features/websites/components/pagination/pagination";
+import type { Meta, StoryFn } from "@storybook/react";
+
+export default {
+  title: "Features/Website/Components/Pagination",
+  component: Pagination,
+} as Meta<typeof Pagination>;
+
+export const Default: StoryFn = (args) => (
+  <Pagination {...args}>
+    <PaginationContent>
+      <PaginationItem>
+        <PaginationPrevious href="#" />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">1</PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#" isActive>
+          2
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">3</PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationEllipsis />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationNext href="#" />
+      </PaginationItem>
+    </PaginationContent>
+  </Pagination>
+);
