0def1e3d5f32f59023bfea1a82d7651794c5c607
Quido Hoekman <quido@qhoekman.nl>
2023-12-29 11:32:10 +0100

    feat(switch): added component

---
+++ src/components/switch/switch.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryFn } from "@storybook/react";
+
+import { Switch } from "./switch";
+
+export default {
+  title: "Components/Data Manipulation/Switch",
+  component: Switch,
+} as Meta<typeof Switch>;
+
+export const Default: StoryFn<typeof Switch> = (args) => (
+  <form>
+    <Switch {...args}></Switch>
+  </form>
+);
