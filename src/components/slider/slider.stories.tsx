f9cb7927c36efb100c902014f7a5803879281bd0
Quido Hoekman <quido@qhoekman.nl>
2023-12-29 11:43:09 +0100

    feat(slider): added component

---
+++ src/components/slider/slider.stories.tsx
@@ -0,0 +1,37 @@
+import { Meta, StoryFn } from '@storybook/react';
+
+import { Slider } from './slider';
+
+export default {
+  title: 'Components/Data Manipulation/Slider',
+  component: Slider,
+  argTypes: {
+    min: {
+      control: {
+        type: 'number',
+      },
+    },
+    max: {
+      control: {
+        type: 'number',
+      },
+    },
+    step: {
+      control: {
+        type: 'number',
+      },
+    },
+  },
+} as Meta<typeof Slider>;
+
+const Template: StoryFn<typeof Slider> = (args) => <Slider {...args}></Slider>;
+
+export const Default = {
+  render: Template,
+  args: {
+    min: 0,
+    max: 100,
+    step: 10,
+    onValueChange: (value: number) => console.log(value),
+  },
+};
