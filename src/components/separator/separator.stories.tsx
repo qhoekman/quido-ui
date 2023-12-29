7d2b9fb46dbb90c331062382c01a8971f0df3191
Quido Hoekman <quido@qhoekman.nl>
2023-12-29 11:35:15 +0100

    feat(separator): added component

---
+++ src/components/separator/separator.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryFn } from "@storybook/react";
+
+import { Separator } from "./separator";
+
+export default {
+  title: "Components/Layout/Separator",
+  component: Separator,
+  argTypes: {
+    orientation: {
+      options: ["horizontal", "vertical"],
+      control: {
+        type: "select",
+      },
+    },
+  },
+} as Meta<typeof Separator>;
+
+export const Default: StoryFn<typeof Separator> = (args) => (
+  <div className="h-[100px]">
+    <Separator {...args}></Separator>
+  </div>
+);
