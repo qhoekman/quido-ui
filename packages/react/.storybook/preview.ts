f53b579c9d8bd8db05cd3aaff5c832d89ef6679c
Quido Hoekman <quido@qhoekman.nl>
2024-02-20 07:29:50 +0100

    chore: added storySort

--- packages/react/.storybook/preview.ts
+++ packages/react/.storybook/preview.ts
@@ -4,6 +4,10 @@ import "../src/index.css";
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    storySort: (a, b) =>
+      a.id === b.id
+        ? 0
+        : a.id.localeCompare(b.id, undefined, { numeric: true }),
     controls: {
       matchers: {
         color: /(background|color)$/i,
