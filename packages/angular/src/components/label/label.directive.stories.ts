61646b7d7acf74632d598591075242d362ee68dd
Quido Hoekman <quido.hoekman@topicus.nl>
2024-07-08 19:30:59 +0200

    feat(angular): added label

---
+++ packages/angular/src/components/label/label.directive.stories.ts
@@ -0,0 +1,29 @@
+import { moduleMetadata, Meta, StoryObj } from '@storybook/angular'
+import { ButtonDirective, ButtonProps } from '@/app/button/button.directive'
+import { CommonModule } from '@angular/common'
+import { LabelDirective } from '@/app/label/label.directive'
+
+type Story = StoryObj<any>
+
+const meta: Meta<any> = {
+  title: 'Components/Data Manipulation/Label',
+  decorators: [
+    moduleMetadata({
+      imports: [CommonModule, LabelDirective]
+    })
+  ],
+  args: {
+    content: 'Label'
+  },
+  render: (args: ButtonProps) => ({
+    props: args,
+    template: `
+<label quiButton>
+{{content}}
+</label>`
+  })
+}
+
+export const Default: Story = {}
+
+export default meta
