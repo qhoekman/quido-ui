a5e804239abf194ac804b653fe54f9e190dde13b
Quido Hoekman <quido@qhoekman.nl>
2025-11-17 07:15:54 +0100

    feat(react): convert switch to styled-components

--- packages/react/src/components/switch/switch.stories.tsx
+++ packages/react/src/components/switch/switch.stories.tsx
@@ -5,7 +5,32 @@ import { Switch } from "./switch";
 export default {
   title: "Components/Data Manipulation/Switch",
   component: Switch,
-} as Meta<typeof Switch>;
+  argTypes: {
+    checked: {
+      control: "boolean",
+    },
+    defaultChecked: {
+      control: "boolean",
+    },
+    disabled: {
+      control: "boolean",
+    },
+    required: {
+      control: "boolean",
+    },
+    name: {
+      control: "text",
+    },
+    value: {
+      control: "text",
+    },
+  },
+  args: {
+    checked: false,
+    disabled: false,
+    required: false,
+  },
+} satisfies Meta<typeof Switch>;
 
 export const Default: StoryFn<typeof Switch> = (args) => (
   <form>
