2b06c54d9b5dd0b76f016c067bcef29c7d654232
Quido Hoekman <quido@qhoekman.nl>
2025-11-19 09:55:01 +0100

    feat(angular): migrate color-picker from ng-pulse to this library

---
+++ packages/angular/src/components/color-picker/color-picker.component.ts
@@ -0,0 +1,168 @@
+import { AsyncPipe } from '@angular/common';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import Color from '@thednp/color';
+import { BehaviorSubject } from 'rxjs';
+import { ColorPaletteComponent } from '../color-palette/color-palette.component';
+import { ColorWheelComponent } from '../color-wheel/color-wheel.component';
+import { TabsContentComponent } from '../tabs/tabs-content.component';
+import { TabsListComponent } from '../tabs/tabs-list.component';
+import { TabsTriggerComponent } from '../tabs/tabs-trigger.component';
+import { TabsComponent } from '../tabs/tabs.component';
+
+@Component({
+  selector: 'pui-color-picker',
+  imports: [
+    TabsComponent,
+    TabsListComponent,
+    TabsTriggerComponent,
+    TabsContentComponent,
+    ColorWheelComponent,
+    ColorPaletteComponent,
+    AsyncPipe,
+  ],
+  standalone: true,
+  template: `
+    @let selectedColor = selectedColorSubject | async;
+    @let selectedVariable = selectedVariableSubject | async;
+    @let displayValues = displayValuesSubject | async;
+    <div data-testid="pui-color-picker" pui-tabs class="tabs">
+      <div data-testid="pui-color-picker-tabs-list" pui-tabs-list>
+        <button data-testid="pui-color-picker-tabs-trigger" pui-tabs-trigger [tabContentFor]="colorWheelContent">
+          Hue
+        </button>
+        <button data-testid="pui-color-picker-tabs-trigger" pui-tabs-trigger [tabContentFor]="colorPaletteContent">
+          Palette
+        </button>
+      </div>
+      <ng-template #colorWheelContent>
+        <div data-testid="pui-color-picker-tabs-content" pui-tabs-content class="tabs-content">
+          <pui-color-wheel
+            [hue]="selectedColor.toHsl().h * 100"
+            [saturation]="selectedColor.toHsl().s * 100"
+            [lightness]="selectedColor.toHsl().l * 100"
+            (valueChange)="handleColorChange($event)"
+          ></pui-color-wheel>
+        </div>
+      </ng-template>
+      <ng-template #colorPaletteContent>
+        <div data-testid="pui-color-picker-tabs-content" pui-tabs-content class="tabs-content">
+          <pui-color-palette [color]="selectedVariable" (valueChange)="handleColorChange($event)"></pui-color-palette>
+        </div>
+      </ng-template>
+    </div>
+    <div data-testid="pui-color-picker-info" class="info">
+      <div data-testid="pui-color-picker-info-color-item" class="color-item">
+        <span class="label">HEX:</span>
+        <pre><code>{{ displayValues?.hex }}</code></pre>
+      </div>
+      <div data-testid="pui-color-picker-info-color-item" class="color-item">
+        <span class="label">HSL:</span>
+        <pre><code>{{ displayValues?.hsl }}</code></pre>
+      </div>
+      <div data-testid="pui-color-picker-info-color-item" class="color-item">
+        <span class="label">RGB:</span>
+        <pre><code>{{ displayValues?.rgb }}</code></pre>
+      </div>
+    </div>
+  `,
+  styles: [
+    `
+      :host {
+        display: block;
+        width: fit-content;
+        border: var(--border-width-default) solid var(--color-border);
+        border-radius: var(--border-radius-sm);
+      }
+
+      :host .tabs {
+        background: transparent;
+      }
+
+      :host .tabs-content {
+        padding: var(--spacing-3) var(--spacing-4);
+      }
+
+      :host .info {
+        display: flex;
+        flex-direction: column;
+        padding: var(--spacing-3) var(--spacing-4);
+        gap: var(--spacing-1);
+      }
+
+      :host .color-item {
+        display: flex;
+        align-items: center;
+        gap: var(--spacing-4);
+      }
+
+      :host .color-item .label {
+        width: var(--spacing-8);
+        font-weight: var(--font-weight-semibold);
+        margin: 0;
+      }
+
+      :host pre {
+        margin: 0;
+      }
+
+      :host code {
+        font-family: var(--font-family-mono);
+        font-size: var(--font-size-xs);
+      }
+    `,
+  ],
+})
+export class ColorPickerComponent implements OnInit, OnChanges {
+  @Input() color = '#000000';
+  @Output() valueChange = new EventEmitter<string>();
+  protected selectedColorSubject = new BehaviorSubject<Color>(new Color('#000000', 'hex'));
+  protected selectedVariableSubject = new BehaviorSubject<string | null>(null);
+  protected displayValuesSubject = new BehaviorSubject<{ hex: string; rgb: string; hsl: string }>({
+    hex: '#000000',
+    rgb: 'rgb(0, 0, 0)',
+    hsl: 'hsl(0, 0%, 0%)',
+  });
+
+  ngOnInit() {
+    this.handleColorChange(this.color);
+
+    this.selectedColorSubject.subscribe((color) => {
+      this.displayValuesSubject.next({
+        hex: this.displayColor('hex', color.toString()),
+        rgb: this.displayColor('rgb', color.toString()),
+        hsl: this.displayColor('hsl', color.toString()),
+      });
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['color']) {
+      this.handleColorChange(changes['color'].currentValue);
+    }
+  }
+
+  getComputedColor(value: string): string {
+    if (!value.startsWith('var(--')) {
+      return value;
+    }
+
+    const computedStyle = getComputedStyle(document.documentElement);
+    const cssVarName = value.slice(4, -1); // Remove var( and )
+    return computedStyle.getPropertyValue(cssVarName).trim();
+  }
+
+  handleColorChange(color: string) {
+    this.selectedVariableSubject.next(null);
+    if (color.startsWith('var(--')) {
+      this.selectedVariableSubject.next(color);
+      color = this.getComputedColor(color);
+    }
+    const newColor = new Color(color, 'hsl');
+    this.selectedColorSubject.next(newColor);
+    this.valueChange.emit(this.selectedColorSubject.toString());
+  }
+
+  displayColor(format: 'hex' | 'rgb' | 'hsl', color: string) {
+    return new Color(color, format).toString();
+  }
+}
