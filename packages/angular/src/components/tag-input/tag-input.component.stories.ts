d6dea9ebf3d0289ec02f05e72db3636335a78f34
Quido Hoekman <quido@qhoekman.nl>
2025-11-19 09:55:04 +0100

    feat(angular): migrate tag-input from ng-pulse to this library

---
+++ packages/angular/src/components/tag-input/tag-input.component.stories.ts
@@ -0,0 +1,40 @@
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+import { TagInputComponent } from './tag-input.component';
+import { StoryComponent } from '../../system/components/story/story.component';
+
+type EnhancedTagInputComponent = TagInputComponent & {
+  tags: string[];
+};
+
+const meta: Meta<EnhancedTagInputComponent> = {
+  title: 'Components/Data Entry/Tag Input',
+  component: TagInputComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [StoryComponent],
+    }),
+  ],
+  argTypes: {
+    tags: {
+      control: 'multi-select',
+      options: ['example', 'tags'],
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<EnhancedTagInputComponent>;
+
+export const Default: Story = {
+  args: {
+    tags: ['example', 'tags'],
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+    <div pui-story style="width: 300px;">
+      <div pui-tag-input [tags]="tags" (tagsChange)="tags = $event" [name]="name" [id]="id">
+      </div>
+    </div>`,
+  }),
+};
