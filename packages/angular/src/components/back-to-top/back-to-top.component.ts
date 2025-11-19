7b91c6526256e8cc2962b6d57f6f5d3c12aabc45
Quido Hoekman <quido@qhoekman.nl>
2025-11-19 09:55:01 +0100

    feat(angular): migrate back-to-top from ng-pulse to this library

---
+++ packages/angular/src/components/back-to-top/back-to-top.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'div[pui-back-to-top]',
+  standalone: true,
+  imports: [CommonModule],
+  host: {
+    'data-testid': 'pui-back-to-top',
+  },
+  template: `<ng-content *ngIf="isVisible"></ng-content>`,
+  styles: [
+    `
+      :host {
+        position: fixed;
+        bottom: var(--spacing-4);
+        right: var(--spacing-4);
+        z-index: var(--z-index-1000);
+      }
+    `,
+  ],
+})
+export class BackToTopComponent {
+  isVisible = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.isVisible = scrollPosition > document.documentElement.clientHeight / 2;
+  }
+
+  @HostListener('click')
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+}
