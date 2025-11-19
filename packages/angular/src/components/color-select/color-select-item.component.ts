871b84ae62b1b5bf1fb67daff130b41ab9552609
Quido Hoekman <quido@qhoekman.nl>
2025-11-19 09:55:01 +0100

    feat(angular): migrate color-select from ng-pulse to this library

---
+++ packages/angular/src/components/color-select/color-select-item.component.ts
@@ -0,0 +1,82 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'button[pui-color-select-item]',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div
+      data-testid="pui-color-select-item-color-indicator"
+      class="color-indicator"
+      [style.backgroundColor]="color"
+      [class.checked]="checked"
+    ></div>
+    <input
+      data-testid="pui-color-select-item-input"
+      type="hidden"
+      [attr.id]="elementRef.nativeElement.id"
+      [attr.name]="elementRef.nativeElement.name"
+      [value]="color"
+    />
+  `,
+  styles: [
+    `
+      :host {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        width: 1.5rem;
+        height: 1.5rem;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: transform 0.2s;
+        border: none;
+        outline: none;
+        padding: 0;
+      }
+
+      :host:focus-visible {
+        box-shadow: 0 0 0 1px var(--color-ring);
+      }
+
+      :host[data-state='checked'] {
+        border: 1px solid var(--color-background-fg);
+        box-shadow: var(--box-shadow-default);
+      }
+
+      :host:disabled {
+        cursor: not-allowed;
+        opacity: 0.5;
+      }
+
+      .color-indicator {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        transition: transform 0.2s;
+      }
+    `,
+  ],
+  host: {
+    type: 'button',
+    role: 'radio',
+    'data-testid': 'pui-color-select-item',
+    '[attr.aria-checked]': 'checked',
+    '[attr.data-state]': 'checked ? "checked" : "unchecked"',
+  },
+})
+export class ColorSelectItemComponent {
+  @Input() checked = false;
+  @Input() color = 'black';
+  @Output() valueSelect = new EventEmitter<string>();
+
+  constructor(public elementRef: ElementRef) {}
+
+  @HostListener('click')
+  toggleCheck() {
+    if (!this.elementRef.nativeElement.disabled) {
+      this.valueSelect.emit(this.color);
+    }
+  }
+}
---
+++ packages/angular/src/components/color-select/color-select.component.stories.ts
@@ -0,0 +1,55 @@
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+
+import { ColorSelectComponent } from './color-select.component';
+import { ColorSelectItemComponent } from './color-select-item.component';
+import { StackComponent } from '../stack/stack.component';
+import { StoryComponent } from '../../system/components/story/story.component';
+
+type EnhancedColorSelectComponent = ColorSelectComponent & {
+  items: { color: string; checked: boolean }[];
+};
+
+const meta: Meta<EnhancedColorSelectComponent> = {
+  title: 'Components/Data Entry/Color Select',
+  component: ColorSelectComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [ColorSelectComponent, ColorSelectItemComponent, StackComponent, StoryComponent],
+    }),
+  ],
+  argTypes: {
+    items: {
+      control: 'object',
+    },
+  } as never,
+};
+export default meta;
+
+type Story = StoryObj<EnhancedColorSelectComponent>;
+
+export const Default: Story = {
+  args: {
+    items: [
+      { color: 'var(--color-red-500)', checked: true },
+      { color: 'var(--color-green-500)', checked: false },
+      { color: 'var(--color-blue-500)', checked: false },
+    ] as never,
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+      <pui-story>
+        <div pui-color-select>
+          <div pui-stack>
+            <button
+              *ngFor="let item of items"
+              pui-color-select-item
+              [color]="item.color"
+              [checked]="item.checked"
+              ></button>
+            </div>
+        </div>
+      </pui-story>
+    `,
+  }),
+};
