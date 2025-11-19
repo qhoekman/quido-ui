93ce27a2c326bf757ef25c91024a4399719d9857
Quido Hoekman <quido@qhoekman.nl>
2025-11-19 09:55:01 +0100

    feat(angular): migrate button from ng-pulse to this library

---
+++ packages/angular/src/components/button/button.component.stories.ts
@@ -0,0 +1,44 @@
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+
+import { ButtonComponent, ButtonVariants } from './button.component';
+import { StoryComponent } from '../../system/components/story/story.component';
+
+const meta: Meta<ButtonComponent> = {
+  title: 'Components/Actions/Button',
+  component: ButtonComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [StoryComponent],
+    }),
+  ],
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: Array<ButtonVariants['variant']>('primary', 'secondary', 'destructive', 'ghost', 'outline', 'link'),
+    },
+    size: {
+      control: 'select',
+      options: Array<ButtonVariants['size']>('sm', 'md', 'lg', 'icon'),
+    },
+    loading: {
+      control: 'boolean',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<ButtonComponent>;
+
+export const Default: Story = {
+  args: {
+    variant: 'primary',
+    size: 'md',
+  },
+  render: (args) => ({
+    props: args,
+    template: `<pui-story><button pui-button [variant]="variant" [size]="size" [loading]="loading" [disabled]="disabled">Button</button></pui-story>`,
+  }),
+};
