bfbc13f49c02aaef391f4e7fe40fd56001cce076
Quido Hoekman <quido@qhoekman.nl>
2025-11-19 08:19:33 +0100

    feat(react): resolve remaining Tailwind classes in stories for avatar

--- packages/react/src/components/avatar/avatar.stories.tsx
+++ packages/react/src/components/avatar/avatar.stories.tsx
@@ -28,6 +28,15 @@ export default {
 export const Default: StoryFn<typeof Avatar> = (args) => {
   return (
     <Avatar {...args}>
+      <AvatarImage src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?auto=format&fit=facearea&w=128&h=128&facepad=2" />
+      <AvatarFallback>JD</AvatarFallback>
+    </Avatar>
+  );
+};
+
+export const Fallback: StoryFn<typeof Avatar> = (args) => {
+  return (
+    <Avatar {...args}>
       <AvatarImage src="/icon.png" />
       <AvatarFallback>JD</AvatarFallback>
     </Avatar>
