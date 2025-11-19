59659a8d60bb2ccea219bba9953d0bee15261f3e
Quido Hoekman <quido@qhoekman.nl>
2025-11-19 09:55:04 +0100

    feat(angular): migrate top-bar from ng-pulse to this library

---
+++ packages/angular/src/components/top-bar/top-bar-section.component.ts
@@ -0,0 +1,101 @@
+import { Component, Input } from '@angular/core';
+
+export type TopBarSectionVariants = {
+  items: 'center' | 'start' | 'end' | 'between' | 'around' | 'initial';
+  align: 'center' | 'start' | 'end' | 'baseline' | 'stretch' | 'initial';
+  direction: 'row' | 'column';
+  gap: 'sm' | 'md' | 'lg';
+};
+
+@Component({
+  selector: 'section[pui-top-bar-section], div[pui-top-bar-section]',
+  standalone: true,
+  host: {
+    '[class]': 'getClassList()',
+    'data-testid': 'pui-top-bar-section',
+  },
+  template: ` <ng-content></ng-content> `,
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex: 1;
+      }
+
+      :host-context(.gap--sm) {
+        gap: var(--spacing-2);
+      }
+
+      :host-context(.gap--md) {
+        gap: var(--spacing-4);
+      }
+
+      :host-context(.gap--lg) {
+        gap: var(--spacing-6);
+      }
+
+      :host-context(.direction--row) {
+        flex-direction: row;
+      }
+
+      :host-context(.direction--column) {
+        flex-direction: column;
+      }
+
+      :host-context(.items--center) {
+        justify-content: center;
+      }
+
+      :host-context(.items--start) {
+        justify-content: flex-start;
+      }
+
+      :host-context(.items--end) {
+        justify-content: flex-end;
+      }
+
+      :host-context(.items--between) {
+        justify-content: space-between;
+      }
+
+      :host-context(.items--around) {
+        justify-content: space-around;
+      }
+
+      :host-context(.align--center) {
+        align-items: center;
+      }
+
+      :host-context(.align--start) {
+        align-items: flex-start;
+      }
+
+      :host-context(.align--end) {
+        align-items: flex-end;
+      }
+
+      :host-context(.align--baseline) {
+        align-items: baseline;
+      }
+
+      :host-context(.align--stretch) {
+        align-items: stretch;
+      }
+    `,
+  ],
+})
+export class TopBarSectionComponent {
+  @Input() items: TopBarSectionVariants['items'] = 'start';
+  @Input() align: TopBarSectionVariants['align'] = 'center';
+  @Input() direction: TopBarSectionVariants['direction'] = 'row';
+  @Input() gap: TopBarSectionVariants['gap'] = 'md';
+
+  getClassList() {
+    return {
+      [`items--${this.items}`]: true,
+      [`align--${this.align}`]: true,
+      [`direction--${this.direction}`]: true,
+      [`gap--${this.gap}`]: true,
+    };
+  }
+}
