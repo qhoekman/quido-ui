0a358a1b969b524bca6ecea3cb02d025ef0e3f13
Quido Hoekman <quido@qhoekman.nl>
2025-11-19 09:55:02 +0100

    feat(angular): migrate loader from ng-pulse to this library

---
+++ packages/angular/src/components/loader/loader.component.ts
@@ -0,0 +1,64 @@
+import { Component, Input } from '@angular/core';
+import { IconUpdateComponent } from '../../icons';
+
+export type LoaderVariants = {
+  variant: 'spinner';
+};
+
+@Component({
+  selector: '[pui-loader]',
+  standalone: true,
+  imports: [IconUpdateComponent],
+  host: {
+    'data-testid': 'pui-loader',
+    '[class]': 'getClassList()',
+  },
+  template: `
+    <ng-content></ng-content>
+    <i *ngIf="active && variant === 'spinner'" pui-icon name="update"> </i>
+  `,
+  styles: [
+    `
+      :host {
+        display: none;
+        transition: opacity 0.3s;
+        opacity: 0;
+      }
+
+      :host(.variant--spinner) {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        white-space: nowrap;
+        gap: var(--spacing-2);
+        border-radius: var(--border-radius-md);
+        animation: spin 3.33s linear infinite;
+      }
+
+      :host(.active) {
+        opacity: 1;
+      }
+
+      @keyframes spin {
+        from {
+          transform: rotate(0deg);
+        }
+
+        to {
+          transform: rotate(360deg);
+        }
+      }
+    `,
+  ],
+})
+export class LoaderComponent {
+  @Input() active = false;
+  @Input() variant: LoaderVariants['variant'] = 'spinner';
+
+  getClassList() {
+    return {
+      active: this.active,
+      'variant--spinner': this.variant === 'spinner',
+    };
+  }
+}
