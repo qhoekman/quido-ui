f10bf89b32197fe90dcc39ec320e98b58c2e7bb5
Quido Hoekman <quido@qhoekman.nl>
2025-11-19 09:55:03 +0100

    feat(angular): migrate steps from ng-pulse to this library

---
+++ packages/angular/src/components/steps/steps-prev.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, HostListener } from '@angular/core';
+import { StepsComponent } from './steps.component';
+
+@Directive({
+  selector: '[puiStepsPrev]',
+  standalone: true,
+  host: {
+    'data-testid': 'pui-steps-prev',
+  },
+})
+export class StepsPrevDirective {
+  constructor(private stepsComponent: StepsComponent) {}
+
+  @HostListener('click')
+  prevStep() {
+    this.stepsComponent.prevStep();
+  }
+}
---
+++ packages/angular/src/components/steps/steps.component.ts
@@ -0,0 +1,64 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Component({
+  selector: 'pui-steps',
+  standalone: true,
+  template: ` <ng-content></ng-content> `,
+  host: {
+    'data-testid': 'pui-steps',
+  },
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+      }
+    `,
+  ],
+})
+export class StepsComponent implements OnInit {
+  @Input() count = 0;
+  private currentStepSubject = new BehaviorSubject<number>(0);
+  private completedStepsSubject = new BehaviorSubject<Set<number>>(new Set());
+
+  currentStep$ = this.currentStepSubject.asObservable();
+  completedSteps$ = this.completedStepsSubject.asObservable();
+
+  ngOnInit() {
+    this.setTotalSteps(this.count);
+  }
+
+  setTotalSteps(count: number) {
+    this.count = count;
+  }
+
+  setStep(step: number) {
+    this.currentStepSubject.next(Math.max(0, Math.min(step, this.count - 1)));
+  }
+
+  nextStep() {
+    const currentStep = this.currentStepSubject.value;
+    if (currentStep < this.count - 1) {
+      this.setStep(currentStep + 1);
+      this.completeStep(currentStep);
+    }
+
+    // if last step, complete all steps
+    if (currentStep === this.count - 1) {
+      this.completeStep(currentStep);
+    }
+  }
+
+  prevStep() {
+    const currentStep = this.currentStepSubject.value;
+    this.setStep(currentStep - 1);
+  }
+
+  completeStep(step: number) {
+    const completedSteps = new Set(this.completedStepsSubject.value);
+    completedSteps.add(step);
+    this.completedStepsSubject.next(completedSteps);
+  }
+}
