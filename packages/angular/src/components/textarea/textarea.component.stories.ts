6f9cdd4ab284945fcf10e51eaddff2c0be953d12
Quido Hoekman <quido@qhoekman.nl>
2025-11-19 09:55:04 +0100

    feat(angular): migrate textarea from ng-pulse to this library

---
+++ packages/angular/src/components/textarea/textarea.component.stories.ts
@@ -0,0 +1,55 @@
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+
+import { LabelComponent } from '../label/label.component';
+import { StackComponent } from '../stack/stack.component';
+import { TextareaComponent } from './textarea.component';
+import { StoryComponent } from '../../system/components/story/story.component';
+
+type EnhancedTextareaComponent = TextareaComponent & {
+  placeholder: string;
+  disabled: boolean;
+};
+
+const meta: Meta<EnhancedTextareaComponent> = {
+  title: 'Components/Data Entry/Textarea',
+  component: TextareaComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [LabelComponent, StackComponent, StoryComponent],
+    }),
+  ],
+  argTypes: {
+    size: {
+      control: 'select',
+      options: ['sm', 'md', 'lg'],
+    },
+    placeholder: {
+      control: 'text',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<EnhancedTextareaComponent>;
+
+export const Default: Story = {
+  args: {
+    size: 'md',
+    placeholder: 'Enter text...',
+    disabled: false,
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+    <pui-story>
+      <div pui-stack direction="column" gap="sm" style="max-width: var(--breakpoint-xs);">
+        <label pui-label [size]="size">Textarea</label>
+        <textarea pui-textarea [size]="size" [placeholder]="placeholder" [disabled]="disabled"></textarea>
+      </div>
+    </pui-story>
+    `,
+  }),
+};
