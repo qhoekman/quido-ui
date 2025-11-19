637bb63b807244ea1cbf28f2b3b2f158eeb4763e
Quido Hoekman <quido@qhoekman.nl>
2025-11-19 09:55:02 +0100

    feat(angular): migrate input from ng-pulse to this library

---
+++ packages/angular/src/components/input/input.component.stories.ts
@@ -0,0 +1,61 @@
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+
+import { LabelComponent } from '../label/label.component';
+import { StackComponent } from '../stack/stack.component';
+import { InputComponent } from './input.component';
+import { StoryComponent } from '../../system/components/story/story.component';
+
+type EnhancedInputComponent = InputComponent & {
+  type: string;
+  placeholder: string;
+  disabled: boolean;
+};
+
+const meta: Meta<EnhancedInputComponent> = {
+  title: 'Components/Data Entry/Input',
+  component: InputComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [LabelComponent, StackComponent, StoryComponent],
+    }),
+  ],
+  argTypes: {
+    size: {
+      control: 'select',
+      options: ['sm', 'md', 'lg'],
+    },
+    type: {
+      control: 'select',
+      options: ['date', 'datetime-local', 'email', 'file', 'month', 'number', 'password', 'text', 'time'],
+    },
+    placeholder: {
+      control: 'text',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<EnhancedInputComponent>;
+
+export const Default: Story = {
+  args: {
+    size: 'md',
+    placeholder: 'Enter text...',
+    type: 'text',
+    disabled: false,
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+      <pui-story>
+        <div pui-stack direction="column" gap="sm" style="max-width: var(--breakpoint-xs);">
+          <label pui-label [size]="size">Label</label>
+          <input pui-input [size]="size" [placeholder]="placeholder" [type]="type" [disabled]="disabled" />
+        </div>
+      </pui-story>
+    `,
+  }),
+};
