c1efeb306e093a5c3ef970cd13702a0780dac21a
Quido Hoekman <quido@qhoekman.nl>
2025-11-19 09:55:01 +0100

    feat(angular): migrate aspect-ratio from ng-pulse to this library

---
+++ packages/angular/src/components/aspect-ratio/aspect-ratio.component.stories.ts
@@ -0,0 +1,32 @@
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+import { SkeletonComponent } from '../skeleton/skeleton.component';
+import { AspectRatioComponent } from './aspect-ratio.component';
+
+const meta: Meta<AspectRatioComponent> = {
+  title: 'Components/Layout/Aspect Ratio',
+  component: AspectRatioComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [AspectRatioComponent, SkeletonComponent],
+    }),
+  ],
+  argTypes: {
+    // Define any controls for the dropdown menu component if needed
+  },
+};
+export default meta;
+
+type Story = StoryObj<AspectRatioComponent>;
+
+export const Default: Story = {
+  args: {},
+
+  render: (args) => ({
+    props: args,
+    template: `
+      <div pui-aspect-ratio="16 / 9">
+        <div pui-skeleton style="width: 100%; height: 120vh;"></div>
+      </div>
+    `,
+  }),
+};
