1c05d96b05a61b46bbdc0aca13f56e6538de6112
Quido Hoekman <quido@qhoekman.nl>
2025-11-19 09:55:01 +0100

    feat(angular): migrate button-group from ng-pulse to this library

---
+++ packages/angular/src/components/button-group/button-group.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'div[pui-button-group]',
+  standalone: true,
+  imports: [CommonModule],
+  host: {
+    'data-testid': 'pui-button-group',
+  },
+  template: `<ng-content></ng-content>`,
+  styles: [
+    `
+      /* stylelint-disable selector-pseudo-element-no-unknown */
+      :host {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        isolation: isolate;
+        border-radius: var(--border-radius-md);
+        box-shadow: var(--box-shadow-default);
+      }
+
+      :host ::ng-deep a,
+      :host ::ng-deep button {
+        position: relative;
+        border-radius: 0;
+        margin: 0;
+      }
+
+      :host ::ng-deep a:first-child,
+      :host ::ng-deep button:first-child {
+        border-top-left-radius: var(--border-radius-md);
+        border-bottom-left-radius: var(--border-radius-md);
+      }
+
+      :host ::ng-deep a:last-child,
+      :host ::ng-deep button:last-child {
+        border-top-right-radius: var(--border-radius-md);
+        border-bottom-right-radius: var(--border-radius-md);
+      }
+    `,
+  ],
+})
+export class ButtonGroupComponent {}
