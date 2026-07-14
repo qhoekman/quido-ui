08c5737595d610bf5aa8c767ceddc4012dd41a06
Quido Hoekman <quido@qhoekman.nl>
2026-07-14 07:59:47 +0200

    feat(react): update wireframe-sphere-background component

---
+++ packages/react/src/features/effects/components/wireframe-sphere-background/wireframe-sphere-background.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryFn } from "@storybook/react-vite";
+
+import { WireframeSphereBackground } from "./wireframe-sphere-background";
+
+export default {
+  title: "Features/Effects/WireframeSphereBackground",
+  component: WireframeSphereBackground,
+  parameters: { layout: "fullscreen" },
+  argTypes: {
+    fixed: { control: "boolean" },
+  },
+  args: {
+    fixed: false,
+  },
+} satisfies Meta<typeof WireframeSphereBackground>;
+
+export const Default: StoryFn<typeof WireframeSphereBackground> = (args) => (
+  <div style={{ width: "100vw", height: "100vh" }}>
+    <WireframeSphereBackground {...args} />
+  </div>
+);
