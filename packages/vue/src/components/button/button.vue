1743ee6c060a1005135fca1547b5817919ef53ac
Quido Hoekman <quido@qhoekman.nl>
2026-07-16 05:57:23 +0200

    feat(vue): update button component

--- packages/vue/src/components/button/button.vue
+++ packages/vue/src/components/button/button.vue
@@ -41,6 +41,7 @@ export interface ButtonProps {
 const props = withDefaults(defineProps<ButtonProps>(), {
   variant: 'primary',
   size: 'md',
+  as: 'button',
   asChild: false,
   disabled: false,
   loading: false,
