1d3da5034a1f8354c08b89768faccc27ec66e154
Quido Hoekman <quido@qhoekman.nl>
2026-07-19 10:46:39 +0200

    feat(react): update separator component

--- packages/react/src/components/separator/separator.tsx
+++ packages/react/src/components/separator/separator.tsx
@@ -1,35 +1,58 @@
 import * as SeparatorPrimitive from "@radix-ui/react-separator";
 import * as React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { cva, type VariantProps } from "class-variance-authority";
 
-const StyledSeparator = styled(SeparatorPrimitive.Root)`
+const separatorVariants = cva("q-separator", {
+  variants: {
+    orientation: {
+      horizontal: "variant--horizontal",
+      vertical: "variant--vertical",
+    },
+  },
+  defaultVariants: {
+    orientation: "horizontal",
+  },
+});
+
+const separatorStyles = css`
+  display: block;
   flex-shrink: 0;
   background-color: var(--color-border);
 
-  &[data-orientation="horizontal"] {
+  &.variant--horizontal {
     height: 1px;
     width: 100%;
   }
 
-  &[data-orientation="vertical"] {
+  &.variant--vertical {
     height: 100%;
     width: 1px;
   }
 `;
 
+const StyledSeparator = styled(SeparatorPrimitive.Root)`
+  ${separatorStyles}
+`;
+
+export type SeparatorProps = React.ComponentPropsWithoutRef<
+  typeof SeparatorPrimitive.Root
+>;
+
 const Separator = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+  SeparatorProps
 >(
   (
     { className, orientation = "horizontal", decorative = true, ...props },
     ref
   ) => (
     <StyledSeparator
+      data-testid="separator"
       ref={ref}
       decorative={decorative}
       orientation={orientation}
-      className={className}
+      className={separatorVariants({ orientation, className })}
       {...props}
     />
   )
