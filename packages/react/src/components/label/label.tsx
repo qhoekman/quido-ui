7b0cbf662879dd2c0cef7225ccf47c3070ec4a23
Quido Hoekman <quido@qhoekman.nl>
2026-07-19 10:46:38 +0200

    feat(react): update label component

--- packages/react/src/components/label/label.tsx
+++ packages/react/src/components/label/label.tsx
@@ -1,19 +1,50 @@
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const labelVariants = cva("q-label");
+const labelVariants = cva("q-label", {
+  variants: {
+    size: {
+      sm: "size--sm",
+      md: "size--md",
+    },
+  },
+  defaultVariants: {
+    size: "sm",
+  },
+});
 
-const StyledLabel = styled(LabelPrimitive.Root)`
-  font-size: var(--font-size-sm);
+const labelStyles = css`
+  display: inline-block;
+  color: var(--color-background-fg);
   font-weight: var(--font-weight-medium);
   line-height: var(--line-height-none);
+
+  &.size--sm {
+    font-size: var(--font-size-sm);
+  }
+
+  &.size--md {
+    font-size: var(--font-size-base);
+  }
 
   .peer:disabled ~ & {
     cursor: not-allowed;
     opacity: 0.7;
   }
+
+  &:has(input:disabled),
+  &:has(textarea:disabled),
+  &:has(+ input:disabled),
+  &:has(+ textarea:disabled) {
+    cursor: not-allowed;
+    opacity: 0.7;
+  }
+`;
+
+const StyledLabel = styled(LabelPrimitive.Root)`
+  ${labelStyles}
 `;
 
 export type LabelProps = LabelPrimitive.LabelProps &
@@ -22,7 +53,12 @@ export type LabelProps = LabelPrimitive.LabelProps &
 export const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   LabelProps
->(({ className, ...props }, ref) => (
-  <StyledLabel ref={ref} className={labelVariants({ className })} {...props} />
+>(({ className, size, ...props }, ref) => (
+  <StyledLabel
+    data-testid="label"
+    ref={ref}
+    className={labelVariants({ size, className })}
+    {...props}
+  />
 ));
 Label.displayName = LabelPrimitive.Root.displayName;
