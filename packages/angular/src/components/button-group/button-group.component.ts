aeed84f680fa75801125224fc05c5f7bc44dc301
Quido Hoekman <quido@qhoekman.nl>
2026-07-21 05:09:38 +0200

    feat(angular): update button-group component

--- packages/angular/src/components/button-group/button-group.component.ts
+++ packages/angular/src/components/button-group/button-group.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   host: {
-    'data-testid': 'qui-button-group',
+    'data-testid': 'button-group',
   },
   template: `<ng-content></ng-content>`,
   styles: [
