5f2100e67a7c65575cf8d3f1e333c6419694f5ed
Quido Hoekman <quido@qhoekman.nl>
2026-07-22 05:55:34 +0200

    feat(angular): update statistic component

--- packages/angular/src/components/statistic/statistic.component.stories.ts
+++ packages/angular/src/components/statistic/statistic.component.stories.ts
@@ -71,3 +71,41 @@ export const Default: Story = {
     `,
   }),
 };
+
+export const Composition: Story = {
+  render: () => ({
+    template: `
+      <qui-story>
+        <div style="display: flex; gap: var(--spacing-8);">
+          <div qui-statistic>
+            <span qui-statistic-label>Active Users</span>
+            <h2 qui-statistic-value>2,338</h2>
+            <div qui-statistic-trend variant="up">
+              <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.14645 2.14645C7.34171 1.95118 7.65829 1.95118 7.85355 2.14645L11.8536 6.14645C12.0488 6.34171 12.0488 6.65829 11.8536 6.85355C11.6583 7.04882 11.3417 7.04882 11.1464 6.85355L8 3.70711L8 12.5C8 12.7761 7.77614 13 7.5 13C7.22386 13 7 12.7761 7 12.5L7 3.70711L3.85355 6.85355C3.65829 7.04882 3.34171 7.04882 3.14645 6.85355C2.95118 6.65829 2.95118 6.34171 3.14645 6.14645L7.14645 2.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+              <span>+180</span>
+            </div>
+            <p qui-statistic-helper-text>New users this week</p>
+          </div>
+          <div qui-statistic>
+            <span qui-statistic-label>Revenue</span>
+            <h2 qui-statistic-value>$48,352</h2>
+            <div qui-statistic-trend variant="up">
+              <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.14645 2.14645C7.34171 1.95118 7.65829 1.95118 7.85355 2.14645L11.8536 6.14645C12.0488 6.34171 12.0488 6.65829 11.8536 6.85355C11.6583 7.04882 11.3417 7.04882 11.1464 6.85355L8 3.70711L8 12.5C8 12.7761 7.77614 13 7.5 13C7.22386 13 7 12.7761 7 12.5L7 3.70711L3.85355 6.85355C3.65829 7.04882 3.34171 7.04882 3.14645 6.85355C2.95118 6.65829 2.95118 6.34171 3.14645 6.14645L7.14645 2.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+              <span>+12%</span>
+            </div>
+            <p qui-statistic-helper-text>Compared to last month</p>
+          </div>
+          <div qui-statistic>
+            <span qui-statistic-label>Churn Rate</span>
+            <h2 qui-statistic-value>2.4%</h2>
+            <div qui-statistic-trend variant="down">
+              <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.5 2C7.77614 2 8 2.22386 8 2.5L8 11.2929L11.1464 8.14645C11.3417 7.95118 11.6583 7.95118 11.8536 8.14645C12.0488 8.34171 12.0488 8.65829 11.8536 8.85355L7.85355 12.8536C7.75979 12.9473 7.63261 13 7.5 13C7.36739 13 7.24021 12.9473 7.14645 12.8536L3.14645 8.85355C2.95118 8.65829 2.95118 8.34171 3.14645 8.14645C3.34171 7.95118 3.65829 7.95118 3.85355 8.14645L7 11.2929L7 2.5C7 2.22386 7.22386 2 7.5 2Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+              <span>-0.3%</span>
+            </div>
+            <p qui-statistic-helper-text>Improved from last month</p>
+          </div>
+        </div>
+      </qui-story>
+    `,
+  }),
+};
