fab0c61c9b372f34ad918572a7c9809ce432118a
Quido Hoekman <quido@qhoekman.nl>
2026-07-22 05:55:41 +0200

    feat(vue): update tree component

--- packages/vue/src/components/tree/tree.stories.ts
+++ packages/vue/src/components/tree/tree.stories.ts
@@ -64,92 +64,6 @@ const meta = {
             </TreeItemComponent>
           </TreeGroupComponent>
         </TreeItemComponent>
-        <TreeItemComponent :default-expanded="args.defaultExpanded">
-          <template #icon>
-            <TreeItemIconComponent>
-              <Folder :size="16" />
-            </TreeItemIconComponent>
-          </template>
-          <template #label>
-            <TreeItemLabelComponent>Downloads</TreeItemLabelComponent>
-          </template>
-          <TreeGroupComponent>
-            <TreeItemComponent>
-              <template #icon>
-                <TreeItemIconComponent>
-                  <File :size="16" />
-                </TreeItemIconComponent>
-              </template>
-              <template #label>
-                <TreeItemLabelComponent>Image.png</TreeItemLabelComponent>
-              </template>
-            </TreeItemComponent>
-            <TreeItemComponent>
-              <template #icon>
-                <TreeItemIconComponent>
-                  <Folder :size="16" />
-                </TreeItemIconComponent>
-              </template>
-              <template #label>
-                <TreeItemLabelComponent>Projects</TreeItemLabelComponent>
-              </template>
-              <TreeGroupComponent>
-                <TreeItemComponent>
-                  <template #icon>
-                    <TreeItemIconComponent>
-                      <File :size="16" />
-                    </TreeItemIconComponent>
-                  </template>
-                  <template #label>
-                    <TreeItemLabelComponent>Project1.zip</TreeItemLabelComponent>
-                  </template>
-                </TreeItemComponent>
-                <TreeItemComponent>
-                  <template #icon>
-                    <TreeItemIconComponent>
-                      <File :size="16" />
-                    </TreeItemIconComponent>
-                  </template>
-                  <template #label>
-                    <TreeItemLabelComponent>Project2.zip</TreeItemLabelComponent>
-                  </template>
-                </TreeItemComponent>
-              </TreeGroupComponent>
-            </TreeItemComponent>
-          </TreeGroupComponent>
-        </TreeItemComponent>
-        <TreeItemComponent :default-expanded="args.defaultExpanded">
-          <template #icon>
-            <TreeItemIconComponent>
-              <Folder :size="16" />
-            </TreeItemIconComponent>
-          </template>
-          <template #label>
-            <TreeItemLabelComponent>Pictures</TreeItemLabelComponent>
-          </template>
-          <TreeGroupComponent>
-            <TreeItemComponent>
-              <template #icon>
-                <TreeItemIconComponent>
-                  <File :size="16" />
-                </TreeItemIconComponent>
-              </template>
-              <template #label>
-                <TreeItemLabelComponent>Vacation.jpg</TreeItemLabelComponent>
-              </template>
-            </TreeItemComponent>
-            <TreeItemComponent>
-              <template #icon>
-                <TreeItemIconComponent>
-                  <File :size="16" />
-                </TreeItemIconComponent>
-              </template>
-              <template #label>
-                <TreeItemLabelComponent>Family.jpg</TreeItemLabelComponent>
-              </template>
-            </TreeItemComponent>
-          </TreeGroupComponent>
-        </TreeItemComponent>
       </TreeComponent>
     `,
   }),
@@ -160,3 +74,142 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const Composition: Story = {
+  render: () => ({
+    components: {
+      TreeComponent,
+      TreeItemComponent,
+      TreeGroupComponent,
+      TreeItemIconComponent,
+      TreeItemLabelComponent,
+      Folder,
+      File,
+    },
+    template: `
+      <div style="max-width: 280px; padding: var(--spacing-4); border: var(--border-width-default) solid var(--color-border); border-radius: var(--border-radius-lg);">
+        <h3 style="margin: 0 0 var(--spacing-3);">File Explorer</h3>
+        <TreeComponent>
+          <TreeItemComponent default-expanded>
+            <template #icon>
+              <TreeItemIconComponent>
+                <Folder :size="16" />
+              </TreeItemIconComponent>
+            </template>
+            <template #label>
+              <TreeItemLabelComponent>Documents</TreeItemLabelComponent>
+            </template>
+            <TreeGroupComponent>
+              <TreeItemComponent>
+                <template #icon>
+                  <TreeItemIconComponent>
+                    <File :size="16" />
+                  </TreeItemIconComponent>
+                </template>
+                <template #label>
+                  <TreeItemLabelComponent>Resume.pdf</TreeItemLabelComponent>
+                </template>
+              </TreeItemComponent>
+              <TreeItemComponent>
+                <template #icon>
+                  <TreeItemIconComponent>
+                    <File :size="16" />
+                  </TreeItemIconComponent>
+                </template>
+                <template #label>
+                  <TreeItemLabelComponent>Cover Letter.docx</TreeItemLabelComponent>
+                </template>
+              </TreeItemComponent>
+            </TreeGroupComponent>
+          </TreeItemComponent>
+          <TreeItemComponent default-expanded>
+            <template #icon>
+              <TreeItemIconComponent>
+                <Folder :size="16" />
+              </TreeItemIconComponent>
+            </template>
+            <template #label>
+              <TreeItemLabelComponent>Downloads</TreeItemLabelComponent>
+            </template>
+            <TreeGroupComponent>
+              <TreeItemComponent>
+                <template #icon>
+                  <TreeItemIconComponent>
+                    <File :size="16" />
+                  </TreeItemIconComponent>
+                </template>
+                <template #label>
+                  <TreeItemLabelComponent>Image.png</TreeItemLabelComponent>
+                </template>
+              </TreeItemComponent>
+              <TreeItemComponent>
+                <template #icon>
+                  <TreeItemIconComponent>
+                    <Folder :size="16" />
+                  </TreeItemIconComponent>
+                </template>
+                <template #label>
+                  <TreeItemLabelComponent>Projects</TreeItemLabelComponent>
+                </template>
+                <TreeGroupComponent>
+                  <TreeItemComponent>
+                    <template #icon>
+                      <TreeItemIconComponent>
+                        <File :size="16" />
+                      </TreeItemIconComponent>
+                    </template>
+                    <template #label>
+                      <TreeItemLabelComponent>Project1.zip</TreeItemLabelComponent>
+                    </template>
+                  </TreeItemComponent>
+                  <TreeItemComponent>
+                    <template #icon>
+                      <TreeItemIconComponent>
+                        <File :size="16" />
+                      </TreeItemIconComponent>
+                    </template>
+                    <template #label>
+                      <TreeItemLabelComponent>Project2.zip</TreeItemLabelComponent>
+                    </template>
+                  </TreeItemComponent>
+                </TreeGroupComponent>
+              </TreeItemComponent>
+            </TreeGroupComponent>
+          </TreeItemComponent>
+          <TreeItemComponent default-expanded>
+            <template #icon>
+              <TreeItemIconComponent>
+                <Folder :size="16" />
+              </TreeItemIconComponent>
+            </template>
+            <template #label>
+              <TreeItemLabelComponent>Pictures</TreeItemLabelComponent>
+            </template>
+            <TreeGroupComponent>
+              <TreeItemComponent>
+                <template #icon>
+                  <TreeItemIconComponent>
+                    <File :size="16" />
+                  </TreeItemIconComponent>
+                </template>
+                <template #label>
+                  <TreeItemLabelComponent>Vacation.jpg</TreeItemLabelComponent>
+                </template>
+              </TreeItemComponent>
+              <TreeItemComponent>
+                <template #icon>
+                  <TreeItemIconComponent>
+                    <File :size="16" />
+                  </TreeItemIconComponent>
+                </template>
+                <template #label>
+                  <TreeItemLabelComponent>Family.jpg</TreeItemLabelComponent>
+                </template>
+              </TreeItemComponent>
+            </TreeGroupComponent>
+          </TreeItemComponent>
+        </TreeComponent>
+      </div>
+    `,
+  }),
+};
